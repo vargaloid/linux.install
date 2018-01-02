92a1b74223708d5fa69e12979edbbb0677e74953
varg <vargaloid@gmail.com>
2018-01-02 15:31:56 +0200

    Visual updates

--- varg.install.sh
+++ varg.install.sh
@@ -11,7 +11,8 @@ if [ "$(id -u)" != "0" ];  then
   echo ""
   exit 1
 else
-  echo " ==================== Hello $(whoami)! ===================="
+  echo ""
+  echo " ====================== Hello $(whoami)! ======================"
   echo " *** This script works only on CentOS 7; Debian 8, Debian 9 ***"
   echo ""
 fi
@@ -21,7 +22,7 @@ if [ -f /etc/redhat-release ]; then
 	OS_RELEASE=$(cat /etc/redhat-release | awk '{print $1}')
 	OS_VERSION=$(cat /etc/os-release | grep VERSION_ID | awk -F '\"' '{print $2}')
 	if [ $OS_RELEASE == CentOS ] && [ $OS_VERSION == 7 ]; then
-		OS="CentOS7"; echo $OS
+		OS="CentOS7"; echo " ========== $OS ========== "
 	else
 		echo "OS not supported!"
 		exit 1
@@ -29,9 +30,9 @@ if [ -f /etc/redhat-release ]; then
 elif [ -f /etc/debian_version ]; then
 	OS_RELEASE=$(lsb_release -c | awk '{print $2}')
 	if [ $OS_RELEASE == jessie ]; then
-		OS="Debian8"; echo "$OS $OS_RELEASE"
+		OS="Debian8"; echo " ======= $OS $OS_RELEASE ======= "
 	elif [ $OS_RELEASE == stretch ]; then
-                OS="Debian9"; echo "$OS $OS_RELEASE"
+                OS="Debian9"; echo " ======= $OS $OS_RELEASE ======= "
 	else
 		echo "OS not supported!"
                 exit 1
