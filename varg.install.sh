78608fb43df27327bfc187e8e684573c32e597ae
varg <vargaloid@gmail.com>
2018-01-02 16:07:05 +0200

    Added Main Menu Added utils installation

--- varg.install.sh
+++ varg.install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 ###############################
-# Installer by Varg. ver 0.02 #
+# Installer by Varg. ver 0.04 #
 ###############################
 
-#check root
+# Check root
 if [ "$(id -u)" != "0" ];  then
   echo ""
   echo " ===== Hello $(whoami)! You need to be root to run this script! ===== "
@@ -17,7 +17,7 @@ else
   echo ""
 fi
 
-#check OS TYPE & VERSION
+# Check OS TYPE & VERSION
 if [ -f /etc/redhat-release ]; then
 	OS_RELEASE=$(cat /etc/redhat-release | awk '{print $1}')
 	OS_VERSION=$(cat /etc/os-release | grep VERSION_ID | awk -F '\"' '{print $2}')
@@ -42,6 +42,40 @@ else
 	exit 1
 fi
 
-#Menu
-echo "What do you want to install?"
-exit 0
+# Main Menu
+echo "----------------------------------------"
+echo "|    What do you want to install?      |"
+echo "----------------------------------------"
+echo "|1. exit                               |"
+echo "|2. mc,vim,sudo,wget,net-tools,git     |"
+echo "|3. zabbix-server                      |"
+echo "|4. OpenVPN                            |"
+echo "----------------------------------------"
+
+read MENU
+
+case $MENU in
+	1)
+ 		echo ""
+		echo " Bye! "
+		echo ""
+		exit 0
+	;;
+	2)
+		if [ "$OS" = "CentOS7" ]; then
+			yum install mc vim sudo wget net-tools git -y
+		else
+			apt-get update
+			apt-get install -y mc vim sudo wget net-tools git
+		fi
+	;;
+	3)
+		echo ""
+                echo "Sorry, but it doesn't ready!"
+                echo ""
+        ;;
+	4)
+		echo ""
+                echo "Sorry, but it doesn't ready!"
+                echo ""        
+esac
