17b4333ae50ecefea97ec721f6c250734309aa92
varg <vargaloid@gmail.com>
2018-04-12 14:33:44 +0300

    Fix Docker installation

--- linux.install.sh
+++ linux.install.sh
@@ -419,16 +419,16 @@ systemctl status docker.service
 		if [ "$OS" = "CentOS7" ]; then
 			yum install -y yum-utils device-mapper-persistent-data lvm2
 			yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-			yum install docker-ce
+			yum install -y docker-ce
 			DockerStart
 			logfile
 		elif [ $OS == "Debian8" || $OS == "Debian9" ]; then
-			apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+			apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 			curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 			apt-key fingerprint 0EBFCD88
 			add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 			apt-get update
-			apt-get install docker-ce
+			apt-get install -y docker-ce
 			DockerStart
 			logfile
                 else
